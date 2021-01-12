2765592b64968e371a0123e6d52e8851a60145c5
RasberryKai <kaithomas2001@gmail.com>
2021-01-12 19:11:00 +0100

    update Tests

---
+++ tests/Remote_test/Remote_test.ino
@@ -0,0 +1,9 @@
+void setup(){
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+}
+
+void loop(){}
+ 
--- tests/other_test/other_test.ino
+++ tests/other_test/other_test.ino
@@ -1,60 +1,78 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
 const int led1 = 5;
 const int led2 = 6;
+const int iregler = 4;
+const int ibewegungsmelder = 7;
 
-void setup(){
-    lcd.begin();
-    Serial.begin(9600);
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
+void setup() {
+  lcd.begin();
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(iregler, OUTPUT);
+  pinMode(ibewegungsmelder, OUTPUT);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  delay(5000);
 }
 
-void loop(){
+void loop() {
+  digitalWrite(iregler, HIGH);
+  Serial.println("Potentiometer Indicator on");
+  delay(2000);
+  digitalWrite(iregler, LOW);
+  Serial.println("Potentiometer Indicator off");
+  delay(2000);
+  digitalWrite(ibewegungsmelder, HIGH);
+  Serial.println("Bewegungsmelder Indicator on");
+  delay(2000);
+  digitalWrite(ibewegungsmelder, LOW);
+  Serial.println("Bewegungsmelder Indicator off");
+  delay(2000);
+  lcd.noBacklight();
+  Serial.println("Display off");
+  delay(2000);
+  lcd.backlight();
+  Serial.println("Display on");
+  delay(1000);
+  for (int i = 0; i <= 3; i++) {
+    lcd.print("abcdefghijklmnop");
+    lcd.setCursor(0, 1);
+    lcd.print("abcdefghijklmnop");
+    lcd.setCursor(0, 2);
+    lcd.print("abcdefghijklmnop");
+    lcd.setCursor(0, 3);
+    lcd.print("abcdefghijklmnop");
+    Serial.println("Printed");
     delay(1000);
-    lcd.noBacklight();
-    Serial.println("Display off");
-    delay(1000);
-    lcd.backlight();
-    Serial.println("Display on");
-    delay(1000);
-    for (int i = 0; i <= 3; i++){
-        lcd.print("abcdefghijklmnop");
-        lcd.setCursor(0, 1);
-        lcd.print("abcdefghijklmnop");
-        lcd.setCursor(0, 2);
-        lcd.print("abcdefghijklmnop");
-        lcd.setCurosr(0, 3);
-        lcd.print("abcdefghijklmnop");
-        Serial.println("Printed");
-        delay(1000);
-        lcd.clear();
-        Serial.println("cleared");
-        delay(1000);
-    }
-    digitalWrite(led1, LOW);
-    Serial.println("Led1 ON");
-    delay(1000);
-    digitalWrite(led1, HIGH);
-    Serial.println("Led1 OFF");
-    delay(1000);
-    digitalWrite(led2, LOW);
-    Serial.println("Led2 ON");
-    delay(1000);
-    digitalWrite(led2, HIGH);
-    Serial.println("Led2 OFF");
-    delay(1000);
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    Serial.println("Both Leds on");
-    delay(1000);
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
     lcd.clear();
-    lcd.noBacklight();
-    Serial.println("Finished");
-    delay(5000);
+    Serial.println("cleared");
+    delay(1000);
+  }
+  digitalWrite(led1, LOW);
+  Serial.println("Led1 ON");
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  Serial.println("Led1 OFF");
+  delay(1000);
+  digitalWrite(led2, LOW);
+  Serial.println("Led2 ON");
+  delay(1000);
+  digitalWrite(led2, HIGH);
+  Serial.println("Led2 OFF");
+  delay(1000);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  Serial.println("Both Leds on");
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  lcd.clear();
+  lcd.noBacklight();
+  Serial.println("Finished");
+  delay(5000);
 }
